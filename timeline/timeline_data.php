2c082c17cb7dc852f0ce7644236aefd9b84ce7f0
ajs6f <ajs6f@d187d172-3112-0410-8ee7-ad866068e984>
2010-04-06 14:00:03 +0000

    From old Timeline plugin.
    git-svn-id: https://addons.omeka.org/svn/plugins/Timeline/trunk@2378 d187d172-3112-0410-8ee7-ad866068e984

--- timeline/timeline_data.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: application/xml');
-echo '<?xml version="1.0" encoding="UTF-8"?>';
-$filename = PLUGIN_DIR . DIRECTORY_SEPARATOR . 'Timeline' . DIRECTORY_SEPARATOR . 'data.xml';
-$handle = fopen($filename, "r");
-$contents = fread($handle, filesize($filename));
-echo $contents;
-fclose($handle);
-?>
