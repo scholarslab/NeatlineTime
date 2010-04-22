e807921ca5c172874cea25819d6a9d0c4168750a
ajs6f <ajs6f@d187d172-3112-0410-8ee7-ad866068e984>
2010-04-22 16:21:54 +0000

    git-svn-id: https://addons.omeka.org/svn/plugins/Timeline/trunk@2444 d187d172-3112-0410-8ee7-ad866068e984

--- plugin.php
+++ plugin.php
@@ -12,6 +12,7 @@ add_plugin_hook('define_routes', 'timeline_routes');
 
 add_filter("show_item_in_page","timeline_show_item_in_page");
 add_filter("item_square_thumbnail","timeline_item_square_thumbnail");
+add_filter("item_has_thumbnail","timeline_item_has_thumbnail");
 
 define('TIMELINE_PLUGIN_VERSION', get_plugin_ini('Timeline', 'version'));
 define('TIMELINE_PLUGIN_DIR', dirname(__FILE__));
@@ -61,6 +62,10 @@ function timeline_show_item_in_page($html, $item){
 		return ""  ;
 	}
 	else return $html;
+}
+
+function timeline_item_has_thumbnail($thumb, $item) {
+	return true;
 }
 
 function timeline_item_square_thumbnail($thumb, $item) {
