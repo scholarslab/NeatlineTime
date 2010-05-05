ef11ea5efaf85acb342a4be5c3a6189a5233fa6e
ajs6f <ajs6f@d187d172-3112-0410-8ee7-ad866068e984>
2010-05-05 15:14:33 +0000

    git-svn-id: https://addons.omeka.org/svn/plugins/Timeline/trunk@2519 d187d172-3112-0410-8ee7-ad866068e984

--- plugin.php
+++ plugin.php
@@ -14,21 +14,23 @@ add_filter("show_item_in_page","timeline_show_item_in_page");
 add_filter("item_square_thumbnail","timeline_item_square_thumbnail");
 add_filter("item_has_thumbnail","timeline_item_has_thumbnail");
 
+add_filter(array('Form','Item','Item Type Metadata','Tag'),"timeline_tag_widget");
+
 define('TIMELINE_PLUGIN_VERSION', get_plugin_ini('Timeline', 'version'));
 define('TIMELINE_PLUGIN_DIR', dirname(__FILE__));
 
 function timeline_install()
 {
 	set_option('timeline_version', TIMELINE_PLUGIN_VERSION);
-	
-# now we add 'Timeline' item type
+
+	# now we add 'Timeline' item type
 	$timelinemitemtype = array(
      'name'       => "Timeline", 
       'description' => "Timeline composed of items in this repository"
       );
 
       $timelinemitemtypemetadata =
-      array(array('name'        => "Tag", 
+      array(array('name'        => "Tag",
               'description' => "Items with this tag should be included in the timeline"             
               ));
               try {
@@ -46,7 +48,7 @@ function timeline_uninstall()
 
 function timeline_initialize()
 {
-	require_once TIMELINE_PLUGIN_DIR . DIRECTORY_SEPARATOR . 'helpers/CreateTimeline.php';	
+	require_once TIMELINE_PLUGIN_DIR . DIRECTORY_SEPARATOR . 'helpers/CreateTimeline.php';
 }
 
 function timeline_show_item_in_page($html, $item){
@@ -70,9 +72,14 @@ function timeline_item_square_thumbnail($thumb, $item) {
 	if ($item->getItemType()->name == "Timeline") {
 		return "<span>Timeline Thumbnail Dummy</span>";
 	}
-	else {		
+	else {
 		return $thumb;
 	}
+}
+
+function timeline_tag_widget($html,$inputNameStem,$value,$options,$record,$element) {
+	$div = __v()->partial('widgets/tag.phtml', array("textarea" => __v()->formTextarea($inputNameStem . "[text]",$value,array('class'=>'textinput', 'rows'=>5, 'cols'=>50))	, "inputNameStem" =>$inputNameStem, "value" => $value, "options" => $options, "record" => $record, "element" => $element));
+	return $div;
 }
 
 // Add the routes from routes.ini in this plugin folder.
