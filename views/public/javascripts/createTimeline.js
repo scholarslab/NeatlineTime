bb339a553a678b7a113944ba43edde0578503d38
ajs6f <ajs6f@d187d172-3112-0410-8ee7-ad866068e984>
2010-06-15 15:27:44 +0000

    git-svn-id: https://addons.omeka.org/svn/plugins/Timeline/trunk@2756 d187d172-3112-0410-8ee7-ad866068e984

--- views/public/javascripts/createTimeline.js
+++ views/public/javascripts/createTimeline.js
@@ -1,26 +1,34 @@
-if (typeof(Omeka) == 'undefined') {
+if (typeof (Omeka) == 'undefined') {
 	Omeka = new Object();
 }
 
 if (!Omeka.Timeline) {
 	Omeka.Timeline = new Array();
 }
+
+if (!Omeka.Timeline.behavior) {
+	Omeka.Timeline.behavior = function(id) {
+	}
+}
+
 Omeka.Timeline.createTimeline = function(event, config) {
 	// we must override this method to provide for our Javascript behaviors on
 	// events
-	Timeline.DefaultEventSource.prototype._resolveRelativeURL = function(url, base) {
-	    if (url == null || url == '') {
-	        return url;
-	    } else if (url.indexOf('://') > 0 || url.indexOf('javascript:') == 0) {
-	        return url;
-	    } else if (url.substr(0,1) == '/') {
-	        return base.substr(0, base.indexOf('/', base.indexOf('://') + 3)) + url;
-	    } else {
-	        return base + url;
-	    }
+	Timeline.DefaultEventSource.prototype._resolveRelativeURL = function(url,
+			base) {
+		if (url == null || url == '') {
+			return url;
+		} else if (url.indexOf('://') > 0 || url.indexOf('javascript:') == 0) {
+			return url;
+		} else if (url.substr(0, 1) == '/') {
+			return base.substr(0, base.indexOf('/', base.indexOf('://') + 3))
+					+ url;
+		} else {
+			return base + url;
+		}
 	};
 	if (!config.eventSource) {
-		config.eventSource = new Timeline.DefaultEventSource();		
+		config.eventSource = new Timeline.DefaultEventSource();
 	}
 	if (!config.bandInfos) {
 		config.bandInfos = [ Timeline.createBandInfo( {
@@ -36,10 +44,16 @@ Omeka.Timeline.createTimeline = function(event, config) {
 		config.bandInfos[1].syncWith = 0;
 		config.bandInfos[1].highlight = true;
 	}
-	Omeka.Timeline[Omeka.Timeline.length] = Timeline.create(config.timelinediv, config.bandInfos);
-	config.eventSource.loadJSON( { "events": config.events }, document.location.href);
-	Omeka.Timeline[Omeka.Timeline.length-1].setCenter = function(s) {
-		this.getBand(0).setCenterVisibleDate(SimileAjax.DateTime.parseGregorianDateTime(s));
+	Omeka.Timeline[Omeka.Timeline.length] = Timeline.create(config.timelinediv,
+			config.bandInfos);
+	config.eventSource.loadJSON( {
+		"events" : config.events
+	}, document.location.href);
+	Omeka.Timeline[Omeka.Timeline.length - 1].setCenter = function(s) {
+		this._bands.each(function(i) {
+			i.setCenterVisibleDate(SimileAjax.DateTime
+					.parseGregorianDateTime(s))
+		});
 	}
 }
 
