7c781c8400a47f22cdcede9fc3f9025c6aa9e61a
ajs6f <ajs6f@d187d172-3112-0410-8ee7-ad866068e984>
2010-08-31 15:23:56 +0000

    git-svn-id: https://addons.omeka.org/svn/plugins/Timeline/trunk@3110 d187d172-3112-0410-8ee7-ad866068e984

--- controllers/TimelinesController.php
+++ controllers/TimelinesController.php
@@ -7,13 +7,10 @@
 class Timeline_TimelinesController extends Omeka_Controller_Action
 
 {
-//	private $logger;
 	
 	public function init()
 	{
 		$this->_modelClass = 'Item';
-//		$writer = new Zend_Log_Writer_Stream(LOGS_DIR . DIRECTORY_SEPARATOR . "timeline.log");
-//		$this->logger = new Zend_Log($writer);
 	}
 
 	public function showAction()
