f3d363b63ff63890204ef818485a43ef57c2e204
ajs6f <ajs6f@d187d172-3112-0410-8ee7-ad866068e984>
2010-09-22 17:14:12 +0000

    git-svn-id: https://addons.omeka.org/svn/plugins/Timeline/trunk@3233 d187d172-3112-0410-8ee7-ad866068e984

--- helpers/CreateTimeline.php
+++ helpers/CreateTimeline.php
@@ -57,7 +57,7 @@ EOT;
 
 	echo "	];
 			Omeka.Timeline.history[Omeka.Timeline.history.length] = TLtmp;	
-			$(document).ready(function (TLtmp) { Omeka.Timeline.createTimeline(this, TLtmp) } );	
+			$(document).ready(function () { Omeka.Timeline.createTimeline(this, TLtmp) } );	
 			delete(TLtmp);
 			Event.observe(document.body, 'resize', onResize);
 		</script>
