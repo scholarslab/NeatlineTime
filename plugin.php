509741890d08eaceedc148c08a0e3052685679ad
wsg4w <wsg4w@d187d172-3112-0410-8ee7-ad866068e984>
2010-10-28 15:03:41 +0000

    Updated documentation on the plugin to denote dependency on theme inclusion of jquery.
    git-svn-id: https://addons.omeka.org/svn/plugins/Timeline/trunk@3475 d187d172-3112-0410-8ee7-ad866068e984

--- plugin.php
+++ plugin.php
@@ -1,8 +1,19 @@
 <?php
 /**
- * @version $Id$
- * @copyright Scholars' Lab
- * @package Timeline
+ * Timeline plugin is a helper function for creating SIMILE Timelines from an
+ * array of items in Omeka. The plugins allows one to specify metadata elements
+ * from which the time data should be derived, as well as the element for
+ * defining the caption.
+ *
+ * This plugin requires that {@link http://www.jquery.org jQuery} be
+ * loaded in your theme (preferably in the head element).
+ *
+ * @author    Scholars Lab 
+ * @copyright 2010 The Board and Visitors of the University of Virginia
+ * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache 2.0
+ * @version   $Id$
+ * @package   Timeline
+ * @link      http://omeka.org/codex/Plugins/Timeline
  **/
 
 add_plugin_hook('install', 'timeline_install');
@@ -20,6 +31,9 @@ add_filter(array('Form','Item','Item Type Metadata','Tag'),"timeline_tag_widget"
 define('TIMELINE_PLUGIN_VERSION', get_plugin_ini('Timeline', 'version'));
 define('TIMELINE_PLUGIN_DIR', dirname(__FILE__));
 
+/**
+ * Install the timeline plugin
+ */
 function timeline_install()
 {
     set_option('timeline_version', TIMELINE_PLUGIN_VERSION);
@@ -42,34 +56,39 @@ function timeline_install()
     }
 }
 
+/**
+ * Uninstall the timeline plugin
+ */
 function timeline_uninstall()
 {
 	delete_option('timeline_plugin_version');
 }
 
+/**
+ * Add the helper functions to the plugin
+ */
 function timeline_initialize()
 {
 	require_once TIMELINE_PLUGIN_DIR . DIRECTORY_SEPARATOR . 'helpers/CreateTimeline.php';
 }
 
-// for actions show and edit, loads the appropriate JS. We need both show and edit to 
-// handle both presentation and editing modes
+/**
+ * Add Timeline javascriprts to the show and edit actions. 
+ */
 function timeline_header()
 {
 	$actionName = Zend_Controller_Front::getInstance()->getRequest()->getActionName();
 	switch (Zend_Controller_Front::getInstance()->getRequest()->getActionName()) {
 		case 'show' :
 		case 'edit' :
-			$j =  js('createTimeline');
+			$timelineFile =  js('createTimeline');
 			print <<<EOT
 <!-- begin Timeline plugin scripts -->
-<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
 <script type="text/javascript">
-    jQuery.noConflict();
     var Timeline_urlPrefix = 'http://static.simile.mit.edu/timeline/api-2.3.1/';
 </script>
 <script type="text/javascript" src="http://static.simile.mit.edu/timeline/api-2.3.1/timeline-api.js"></script>
-$j
+$timelineFile
 <!-- end Timeline plugin scripts -->
 EOT;
 			break;
@@ -77,6 +96,14 @@ EOT;
 	}
 }
 
+/**
+ * Inject timeline-formatted objects in to the page
+ *
+ * @param string $html
+ * @param string $item
+ * @param array $options
+ * @return string 
+ */
 function timeline_show_item_in_page($html, $item, $options = array()) {
     if ($item->getItemType()->name == "Timeline") {
         $CONFIG = array(
@@ -98,10 +125,22 @@ function timeline_show_item_in_page($html, $item, $options = array()) {
      else return $html;
 }
 
+/**
+ *
+ * @param <type> $thumb
+ * @param <type> $item
+ * @return <type>
+ */
 function timeline_item_has_thumbnail($thumb, $item) {
     return true;
 }
 
+/**
+ *
+ * @param <type> $thumb
+ * @param <type> $item
+ * @return <type> 
+ */
 function timeline_item_square_thumbnail($thumb, $item) {
     $item = $item ? $item : get_item_by_id(item('ID'));
 
@@ -112,13 +151,27 @@ function timeline_item_square_thumbnail($thumb, $item) {
     }
 }
 
+/**
+ *
+ * @param <type> $html
+ * @param <type> $inputNameStem
+ * @param <type> $value
+ * @param <type> $options
+ * @param <type> $record
+ * @param <type> $element
+ * @return <type> 
+ */
 function timeline_tag_widget($html,$inputNameStem,$value,$options,$record,$element)
 {
     $div = __v()->partial('widgets/tag.phtml', array("inputNameStem" =>$inputNameStem, "value" => $value, "options" => $options, "record" => $record, "element" => $element));
     return $div;
 }
 
-// Add the routes from routes.ini in this plugin folder.
+/**
+ * Add the routes from routes.ini in this plugin folder.
+ *
+ * @param <type> $router 
+ */
 function timeline_routes($router)
 {
     $router->addConfig(new Zend_Config_Ini(TIMELINE_PLUGIN_DIR .
