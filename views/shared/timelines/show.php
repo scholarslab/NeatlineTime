13d3bd9906506389f6acfebc2ee042df11cb327b
Jeremy Boggs <jeremy@clioweb.org>
2011-06-15 09:08:53 -0400

    Adds comment to explain use of Zend's headScript to add Simile Timeline JS.

--- views/shared/timelines/show.php
+++ views/shared/timelines/show.php
@@ -1,4 +1,11 @@
 <?php
+/*
+Add the Simile Timeline javascript to the queue using Zend's headScript,
+since Omeka's queue_js() function doesn't support URLs.
+
+Must call both javascripts before calling head() function, so the scripts
+get added to the queue before the page loads.
+*/
 $this->headScript()->appendFile('http://static.simile.mit.edu/timeline/api-2.3.1/timeline-api.js?bundle=false');
 queue_js('createTimeline');
 ?>
