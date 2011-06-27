06c02116a9e83004edd2781747d5987b0f3aeb17
Jeremy Boggs <jeremy@clioweb.org>
2011-06-27 11:23:37 -0400

    Updates function names in AclTest.

--- tests/cases/AclTest.php
+++ tests/cases/AclTest.php
@@ -9,36 +9,28 @@
  */
 
 /**
- * @package Timeline
+ * @package     Timeline
  * @copyright   2010-2011 The Board and Visitors of the University of Virginia
  */
 class Timeline_AclTest extends Omeka_Test_AppTestCase
 {
     const RESOURCE = 'Timeline_Timelines';
-    
+
     public function setUp()
     {
         parent::setUp();
         $this->helper = new Timeline_IntegrationHelper;
         $this->helper->setUpPlugin();
     }
-    
+
     public function assertPreConditions()
     {
         $this->assertTrue($this->acl->has(self::RESOURCE));
     }
-    
-    public function testUnauthenticatedUsersCannotAddOrEditExhibits()
+
+    public function testUnauthenticatedUsersCannotAddOrEditTimelines()
     {
         $this->assertFalse($this->acl->isAllowed(null, self::RESOURCE, 'add'));
         $this->assertFalse($this->acl->isAllowed(null, self::RESOURCE, 'edit'));
     }
-    
-    public function testContributorUsersCanAddAndEditOwnExhibits()
-    {
-        $this->assertTrue($this->acl->isAllowed('contributor', self::RESOURCE, 'add'));
-        $this->assertFalse($this->acl->isAllowed('contributor', self::RESOURCE, 'editAll'));
-        $this->assertTrue($this->acl->isAllowed('contributor', self::RESOURCE, 'editSelf'));
-    }
-    
 }
