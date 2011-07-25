51eb12a1c8cdcc37d9d96f72223e7cbee99aa09b
Jeremy Boggs <jeremy@clioweb.org>
2011-07-25 16:46:41 -0400

    Adds quasi-working Timeglider JSON output for individual timelines.

--- views/admin/timelines/show.php
+++ views/admin/timelines/show.php
@@ -23,11 +23,11 @@ head($head);
     <script>
     jQuery(document).ready(function($){
         $('#timeglider')
-            .css({'height': '300px', 'margin-bottom': '20px'})
+            .css({'height': '400px', 'margin-bottom': '20px'})
             .timeline({
-                "data_source":"http://omeka.dev/admin/items/browse?search=&advanced%5B0%5D%5Belement_id%5D=40&advanced%5B0%5D%5Btype%5D=is+not+empty&advanced%5B0%5D%5Bterms%5D=&range=&collection=&type=&user=&tags=&public=&featured=&contributed=&submit_search=Search&output=timeglider-json",
-                "min_zoom":1,
-                "max_zoom":50,
+                "data_source": <?php echo js_escape(abs_timeline_uri().'?output=timeglider-json'); ?>,
+                "min_zoom":5,
+                "max_zoom":60,
                 "show_footer": false
             });
     });
--- views/shared/timelines/show.timeglider-json.php
+++ views/shared/timelines/show.timeglider-json.php
@@ -10,3 +10,53 @@
  * @link http://omeka.org/codex/Plugins/Timeline
  * @since 1.0
  */
+
+$timegliderJsonArray = array(
+    'id' => 'timeline-'.timeline('id'),
+    'title' => timeline('title'),
+    'initial_zoom' => '40'
+);
+
+$timegliderJsonEventsArray = array();
+
+// Retrieve all TimelineEntry records for the current Timeline.
+$timelineEntries = $timeline->getTimelineEntries();
+
+foreach ($timelineEntries as $entry) {
+
+    // If the entry has stuff in its data column.
+    if ($data = $entry->data) {
+
+        // If $data is numeric, we'll check to see if we can get an Item with
+        // that ID number.
+        if (is_numeric($data) && $item = get_item_by_id($data)) {
+
+            set_current_item($item);
+            $jsonData = array(
+                'title' => item('Dublin Core', 'Title'),
+                'startdate'     => date('Y-m-d H:m:s', strtotime(item('Dublin Core', 'Date')))
+            );
+
+        } else {
+
+            $jsonData = unserialize($data);
+
+        }
+
+        // Set a unique ID for the timeglider JSON entry.
+        $jsonData['id'] = 'timeline-entry-'.$entry->id;
+
+        // Add the jsonData to the Timeglider JSON events array.
+        $timegliderJsonEventsArray[] = $jsonData;
+    }
+}
+
+// If the Timeglider JSON events array isn't empty, add it to the overall array.
+if (!empty($timegliderJsonEventsArray)) {
+
+    $timegliderJsonArray['events'] = $timegliderJsonEventsArray;
+
+}
+
+// Roll that beautiful bean footage.
+echo '['.json_encode($timegliderJsonArray).']';
