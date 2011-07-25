cb219eb66e8440693b3827c7f899a32951ba91b5
Jeremy Boggs <jeremy@clioweb.org>
2011-07-25 16:44:13 -0400

    Creates TimelineEntryTable model.

---
+++ models/TimelineEntryTable.php
@@ -0,0 +1,27 @@
+<?php 
+/**
+ * @author Scholars' Lab
+ * @copyright 2011 The Board and Visitors of the University of Virginia
+ * @license http://www.apache.org/licenses/LICENSE-2.0 Apache 2.0
+ * @package Timeline
+ * @link http://omeka.org/codex/Plugins/Timeline
+ * @since 1.0
+ */
+
+/**
+ * Timeline Entry Table class.
+ *
+ * @since 1.0
+ * @author Scholars' Lab
+ * @package Timeline
+ * @subpackage Models
+ */
+class TimelineEntryTable extends Omeka_Db_Table
+{
+    public function findByTimeline(Omeka_Record $timeline)
+    {
+        $select = $this->getSelect();
+        $select->where('`timeline_id` = ?', (int)$timeline->id);
+        return $this->fetchObjects($select);
+    }
+}
