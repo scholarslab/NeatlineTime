c38a373e7682c67c8685ba584b2647c7d8ba1cac
David McClure <david.mcclure@virginia.edu>
2011-08-25 15:07:07 -0400

    Revert "name change in plugin.php"
    This reverts commit 550555939e0797a20e1631005d4112e39b3a0203.

--- plugin.php
+++ plugin.php
@@ -3,24 +3,24 @@
  * @author Scholars' Lab
  * @copyright 2010-2011 The Board and Visitors of the University of Virginia
  * @license http://www.apache.org/licenses/LICENSE-2.0 Apache 2.0
- * @package Neatline Time
+ * @package Timeline
  * @link http://omeka.org/codex/Plugins/Timeline
  * @since 1.0
  */
 
-if (!defined('NEATLINE_TIME_PLUGIN_DIR')) {
-    define('NEATLINE_TIME_PLUGIN_DIR', dirname(__FILE__));
+if (!defined('TIMELINE_PLUGIN_DIR')) {
+    define('TIMELINE_PLUGIN_DIR', dirname(__FILE__));
 }
 
-if (!defined('NEATLINE_TIME_HELPERS_DIR')) {
-    define('NEALTINE_TIME_HELPERS_DIR', NEATLINE_TIME_PLUGIN_DIR . '/helpers');
+if (!defined('TIMELINE_HELPERS_DIR')) {
+    define('TIMELINE_HELPERS_DIR', TIMELINE_PLUGIN_DIR . DIRECTORY_SEPARATOR . 'helpers');
 }
 
-if (!defined('NEATLINE_TIME_FORMS_DIR')) {
-    define('NEATLINE_TIME_FORMS_DIR', NEATLINE_TIME_PLUGIN_DIR . '/forms');
+if (!defined('TIMELINE_FORMS_DIR')) {
+    define('TIMELINE_FORMS_DIR', TIMELINE_PLUGIN_DIR . DIRECTORY_SEPARATOR . 'forms');
 }
 
-require_once NEATLINE_TIME_PLUGIN_DIR . '/TimelinePlugin.php';
-require_once NEATLINE_TIME_PLUGIN_DIR . '/TimelineFunctions.php';
+require_once TIMELINE_PLUGIN_DIR . DIRECTORY_SEPARATOR . 'TimelinePlugin.php';
+require_once TIMELINE_HELPERS_DIR . DIRECTORY_SEPARATOR . 'TimelineFunctions.php';
 
 new TimelinePlugin;
