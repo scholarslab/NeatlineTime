ff9be4c33a828e90aa907f61fe301c7f0f89d392
David McClure <david.mcclure@virginia.edu>
2011-10-04 11:13:03 -0400

    plugin.php merge

--- plugin.php
+++ plugin.php
@@ -8,25 +8,16 @@
  * @since 1.0
  */
 
-if (!defined('TIMELINE_PLUGIN_DIR')) {
-    define('TIMELINE_PLUGIN_DIR', dirname(__FILE__));
+if (!defined('NEATLINE_TIME_PLUGIN_DIR')) {
+    define('NEATLINE_TIME_PLUGIN_DIR', dirname(__FILE__));
 }
 
-<<<<<<< HEAD
-if (!defined('TIMELINE_HELPERS_DIR')) {
-<<<<<<< HEAD
-    define('TIMELINE_HELPERS_DIR', TIMELINE_PLUGIN_DIR . '/helpers');
-=======
 if (!defined('NEATLINE_TIME_HELPERS_DIR')) {
     define('NEATLINE_TIME_HELPERS_DIR', NEATLINE_TIME_PLUGIN_DIR . '/helpers');
->>>>>>> rename
-=======
-    define('TIMELINE_HELPERS_DIR', TIMELINE_PLUGIN_DIR . DIRECTORY_SEPARATOR . 'helpers');
->>>>>>> parent of eef36ff... got rid of DIR_SEPARATORS
 }
 
-if (!defined('TIMELINE_FORMS_DIR')) {
-    define('TIMELINE_FORMS_DIR', TIMELINE_PLUGIN_DIR . DIRECTORY_SEPARATOR . 'forms');
+if (!defined('NEATLINE_TIME_FORMS_DIR')) {
+    define('NEATLINE_TIME_FORMS_DIR', NEATLINE_TIME_PLUGIN_DIR . '/forms');
 }
 
 require_once NEATLINE_TIME_PLUGIN_DIR . '/NeatlineTimePlugin.php';
