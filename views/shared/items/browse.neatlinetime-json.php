9d982db25cc38c42f80a6509b56930733153ee24
Jeremy Boggs <jeremy@clioweb.org>
2011-11-21 10:29:56 -0500

    Filename changes for item json files.
    Updates header doc for each file.

--- views/shared/items/browse.neatlinetime-json.php
+++ views/shared/items/browse.neatlinetime-json.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * The shared timeglider-json browse view for Items
+ * The shared neatlinetime-json browse view for Items
  *
  * @author Scholars' Lab
  * @copyright 2011 The Board and Visitors of the University of Virginia
  * @license http://www.apache.org/licenses/LICENSE-2.0 Apache 2.0
- * @package Timeline
+ * @package NeatlineTime
  * @subpackage Views
- * @link http://omeka.org/codex/Plugins/Timeline
+ * @link http://github.com/scholarslab/NeatlineTime
  * @since 1.0
  */
