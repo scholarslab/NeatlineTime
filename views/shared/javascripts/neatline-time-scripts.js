f7e2e17552b436fcaceafeca5577468b98762f3f
Jeremy Boggs <jeremy@clioweb.org>
2011-12-05 15:08:30 -0500

    Fixes #18, adds zooming to timeline.
    Users can zoom in on a timeline band using mousewheel.

--- views/shared/javascripts/neatline-time-scripts.js
+++ views/shared/javascripts/neatline-time-scripts.js
@@ -1,6 +1,6 @@
 var NeatlineTime = {
   resizeTimerID: null,
-  
+
   resizeTimeline: function() {
      if (resizeTimerID == null) {
         resizeTimerID = window.setTimeout(function() {
@@ -13,17 +13,34 @@ var NeatlineTime = {
   loadTimeline: function(timelineId, timelineData) {
     var eventSource = new Timeline.DefaultEventSource();
 
+    var defaultTheme = Timeline.getDefaultTheme();
+    defaultTheme.mouseWheel = 'zoom';
+
     var bandInfos = [
         Timeline.createBandInfo({
             eventSource:    eventSource,
-            width:          "70%",
+            width:          "80%",
             intervalUnit:   Timeline.DateTime.MONTH,
-            intervalPixels: 100
+            intervalPixels: 100,
+            zoomIndex:      10,
+            zoomSteps:      new Array(
+              {pixelsPerInterval: 280,  unit: Timeline.DateTime.HOUR},
+              {pixelsPerInterval: 140,  unit: Timeline.DateTime.HOUR},
+              {pixelsPerInterval:  70,  unit: Timeline.DateTime.HOUR},
+              {pixelsPerInterval:  35,  unit: Timeline.DateTime.HOUR},
+              {pixelsPerInterval: 400,  unit: Timeline.DateTime.DAY},
+              {pixelsPerInterval: 200,  unit: Timeline.DateTime.DAY},
+              {pixelsPerInterval: 100,  unit: Timeline.DateTime.DAY},
+              {pixelsPerInterval:  50,  unit: Timeline.DateTime.DAY},
+              {pixelsPerInterval: 400,  unit: Timeline.DateTime.MONTH},
+              {pixelsPerInterval: 200,  unit: Timeline.DateTime.MONTH},
+              {pixelsPerInterval: 100,  unit: Timeline.DateTime.MONTH} // DEFAULT zoomIndex
+            )
         }),
         Timeline.createBandInfo({
             overview:       true,
             eventSource:    eventSource,
-            width:          "30%",
+            width:          "20%",
             intervalUnit:   Timeline.DateTime.YEAR,
             intervalPixels: 200
         })
