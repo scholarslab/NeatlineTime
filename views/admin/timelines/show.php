a98561b43900cb05609920f4049d7ea5f45c5806
Jeremy Boggs <jeremy@clioweb.org>
2011-12-20 12:04:54 -0500

    Adds edit buttons to timelines show in admin.

--- views/admin/timelines/show.php
+++ views/admin/timelines/show.php
@@ -20,7 +20,12 @@ $head = array('bodyclass' => 'timelines primary',
 head($head);
 ?>
 <h1><?php echo timeline('title'); ?> <span class="view-public-page">[ <a href="<?php echo html_escape(public_uri('neatline-time/timelines/show/'.timeline('id'))); ?>">View Public Page</a> ]</h1>
-
+<?php if (has_permission(get_current_timeline(), 'edit')): ?>
+<p id="edit-timeline" class="edit-button">
+    <?php echo link_to_timeline('Edit Metadata', array('class' => 'edit'), 'edit'); ?>
+    <?php echo link_to_timeline('Edit Items Query', array('class' => 'edit'), 'query'); ?>
+</p>
+<?php endif; ?>
 <div id="primary">
     <?php echo neatlinetime_display_timeline(); ?>
     <?php echo timeline('description'); ?>
