ed94fedeeb575b72055d8974f4fe67a67fd56a6b
Jeremy Boggs <jeremy@clioweb.org>
2012-01-03 13:57:21 -0500

    Simplifies data for routing test, adds show to list of routes to test.

--- tests/integration/Controllers/TimelinesControllerTest.php
+++ tests/integration/Controllers/TimelinesControllerTest.php
@@ -19,20 +19,21 @@ class TimelinesControllerTest extends NeatlineTime_Test_AppTestCase
     public static function routes()
     {
         return array(
-            array('/neatline-time/timelines/browse', 'timelines', 'browse'),
-            array('/neatline-time/timelines/add', 'timelines', 'add'),
-            array('/neatline-time/timelines/edit/1', 'timelines', 'edit'),
-            array('/neatline-time/timelines/query/1', 'timelines', 'query')
+            array('/neatline-time/timelines/browse', 'browse'),
+            array('/neatline-time/timelines/add', 'add'),
+            array('/neatline-time/timelines/show/1', 'show'),
+            array('/neatline-time/timelines/edit/1', 'edit'),
+            array('/neatline-time/timelines/query/1', 'query')
         );
     }
 
     /**
      * @dataProvider routes
      */
-    public function testRouting($url, $controller, $action, $callback = null)
+    public function testRouting($url, $action)
     {
-        $this->dispatch($url, $callback);
-        $this->assertController($controller);
+        $this->dispatch($url);
+        $this->assertController('timelines');
         $this->assertAction($action);
     }
 
