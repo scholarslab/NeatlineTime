695faa0159e60ed18cad556915b5104a464b4581
Jeremy Boggs <jeremy@clioweb.org>
2012-05-16 14:59:04 -0400

    More reliably set the timeline center date.
    Uses method in SIMILE Timeline library for setting the center date for a band,
    based on the earliest event loaded to the timeline.
    
    Removes unreliable function for sorting and retrieving the first item for a
    given query.

--- views/shared/timelines/_timeline.php
+++ views/shared/timelines/_timeline.php
@@ -6,14 +6,11 @@
 
 <!-- Container. -->
 <div id="<?php echo neatlinetime_timeline_id(); ?>" class="neatlinetime-timeline"></div>
-<?php neatlinetime_get_startdate(); ?>
 <script>
     jQuery(document).ready(function($) {
         NeatlineTime.loadTimeline(
             '<?php echo neatlinetime_timeline_id(); ?>',
-            '<?php echo neatlinetime_json_uri_for_timeline(); ?>',
-            '<?php echo neatlinetime_get_startdate(); ?>'
-
+            '<?php echo neatlinetime_json_uri_for_timeline(); ?>'
         );
     });
 </script>
