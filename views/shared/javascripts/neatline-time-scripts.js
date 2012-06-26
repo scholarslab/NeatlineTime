107a9d87767db1cfa85dbb0c2614875541ec85fd
Eric Rochester <err8n@virginia.edu>
2012-06-26 11:03:53 -0400

    Monkey patch SIMILE's fillInfoBubble handler to allow HTML in titles.
    Fixes #35.

--- views/shared/javascripts/neatline-time-scripts.js
+++ views/shared/javascripts/neatline-time-scripts.js
@@ -10,7 +10,58 @@ var NeatlineTime = {
     }
   },
 
+  _monkeyPatchFillInfoBubble: function() {
+      var oldFillInfoBubble =
+          Timeline.DefaultEventSource.Event.prototype.fillInfoBubble;
+      Timeline.DefaultEventSource.Event.prototype.fillInfoBubble =
+          function(elmt, theme, labeller) {
+          var doc = elmt.ownerDocument;
+
+          var title = this.getText();
+          var link = this.getLink();
+          var image = this.getImage();
+
+          if (image != null) {
+              var img = doc.createElement("img");
+              img.src = image;
+
+              theme.event.bubble.imageStyler(img);
+              elmt.appendChild(img);
+          }
+
+          var divTitle = doc.createElement("div");
+          var textTitle = doc.createElement("span");
+          textTitle.innerHTML = title;
+          if (link != null) {
+              var a = doc.createElement("a");
+              a.href = link;
+              a.appendChild(textTitle);
+              divTitle.appendChild(a);
+          } else {
+              divTitle.appendChild(textTitle);
+          }
+          theme.event.bubble.titleStyler(divTitle);
+          elmt.appendChild(divTitle);
+
+          var divBody = doc.createElement("div");
+          this.fillDescription(divBody);
+          theme.event.bubble.bodyStyler(divBody);
+          elmt.appendChild(divBody);
+
+          var divTime = doc.createElement("div");
+          this.fillTime(divTime, labeller);
+          theme.event.bubble.timeStyler(divTime);
+          elmt.appendChild(divTime);
+
+          var divWiki = doc.createElement("div");
+          this.fillWikiInfo(divWiki);
+          theme.event.bubble.wikiStyler(divWiki);
+          elmt.appendChild(divWiki);
+      };
+  },
+
   loadTimeline: function(timelineId, timelineData) {
+    NeatlineTime._monkeyPatchFillInfoBubble();
     var eventSource = new Timeline.DefaultEventSource();
 
     var defaultTheme = Timeline.getDefaultTheme();
