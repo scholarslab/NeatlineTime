b0164477e924307f58b9971646fbb6186112a85a
Wayne Graham <wayne.graham@virginia.edu>
2012-08-21 09:26:32 -0400

    Remove irc notice while debugging; updated README, remove extra cd

--- travis_setup.sh
+++ travis_setup.sh
@@ -5,15 +5,14 @@ if [ -z $OMEKA_BRANCH ]; then
 fi
 
 export OMEKA_DIR=`pwd`/omeka
+echo $OMEKA_DIR
 
-$DBNAME=omeka_test
-
-mysql -e "create database IF NOT EXISTS $DBNAME;" -uroot;
+mysql -e "create database IF NOT EXISTS omeka_test;" -uroot;
 git clone https://github.com/omeka/Omeka.git $OMEKA_DIR
 
 # check out the correct branch
 cd $OMEKA_DIR && git checkout $OMEKA_BRANCH
-cd
+
 # move configuration files
 mv $OMEKA_DIR/application/config/config.ini.changeme $OMEKA_DIR/application/config/config.ini
 mv $OMEKA_DIR/application/tests/config.ini.changeme $OMEKA_DIR/application/tests/config.ini
@@ -21,7 +20,7 @@ mv $OMEKA_DIR/application/tests/config.ini.changeme $OMEKA_DIR/application/tests
 # set up testing config
 sed -i 's/db.host = ""/db.host = "localhost"/' $OMEKA_DIR/application/tests/config.ini
 sed -i 's/db.username = ""/db.username = "root"/' $OMEKA_DIR/application/tests/config.ini
-sed -i 's/db.dbname = ""/db.dbname = "omeka_test"/' $OMEKA_DIR/application/tests/config.in 
+sed -i 's/db.dbname = ""/db.dbname = "omeka_test"/' $OMEKA_DIR/application/tests/config.ini 
 sed -i 's/email.to = ""/email.to = "test@example.com"/' $OMEKA_DIR/application/tests/config.ini
 sed -i 's/email.administator = ""/email.administrator = "admin@example.com"/' $OMEKA_DIR/application/tests/config.ini
 sed -i 's/paths.maildir = ""/paths.maildir = "\/tmp"/' $OMEKA_DIR/application/tests/config.ini
