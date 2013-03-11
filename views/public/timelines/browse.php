3c4a43c883c394dde913f18fd875dbd0fb9b7f58
Jeremy Boggs <jeremy@clioweb.org>
2013-03-11 11:57:17 -0400

    Updates public timelines/browse.

--- views/public/timelines/browse.php
+++ views/public/timelines/browse.php
@@ -10,12 +10,13 @@ echo head($head);
 
 <div class="timelines">
 <h1><?php echo __('Browse Timelines'); ?></h1>
-    <?php if (has_timelines_for_loop()) : while ( loop_timelines() ) :?>
+    <?php if ($total_results) : ?>
+    <?php foreach (loop('Neatline_Time_Timelines') as $timeline): ?>
     <div class="timeline">
         <h2><?php echo link_to_timeline(); ?></h2>
-        <?php echo timeline('Description'); ?>
+        <?php echo snippet_by_word_count(metadata($timeline, 'description'), '10'); ?>
     </div>
-    <?php endwhile; ?>
+    <?php endforeach; ?>
     <div class="pagination">
       <?php echo pagination_links(); ?>
     </div>
