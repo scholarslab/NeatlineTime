f867b660dc243fe3501d3e33d7461841ae37c39d
Eric Rochester <erochest@virginia.edu>
2014-10-02 11:31:37 -0400

    Don't use deprecated `assertTag`.

--- tests/integration/Helpers/LinkToTimelineTest.php
+++ tests/integration/Helpers/LinkToTimelineTest.php
@@ -18,27 +18,28 @@ class LinkToTimelineTest extends NeatlineTime_Test_AppTestCase
 
         $this->dispatch('neatline-time/timelines/show/1');
 
-        $matcher = array(
-            'tag' => 'a',
-            'content' => $timeline->title,
-            'attributes' => array(
-                'href' => record_url($timeline)
-            )
-        );
+        $content = $timeline->title;
+        $url     = record_url($timeline);
 
         $linkDefault = link_to_timeline();
-        $this->assertTag($matcher, $linkDefault);
+        $this->assertEquals("<a href=\"$url\">$content</a>", $linkDefault);
 
         $linkText = 'New Text';
         $linkWithNewText = link_to_timeline($linkText);
         $matcher['content'] = $linkText;
-        $this->assertTag($matcher, $linkWithNewText);
+        $this->assertEquals("<a href=\"$url\">$linkText</a>", $linkWithNewText);
 
-        $linkToEditWithProps = link_to_timeline(null, array('class' => 'edit'), 'edit');
-        $matcher['content'] = $timeline->title;
-        $matcher['attributes']['class'] = 'edit';
-        $matcher['attributes']['href'] = record_url($timeline, 'edit');
-        $this->assertTag($matcher, $linkToEditWithProps);
+        $url = record_url($timeline, 'edit');
+
+        $linkToEditWithProps = link_to_timeline(
+            null,
+            array('class' => 'edit'),
+            'edit'
+        );
+        $this->assertEquals(
+            "<a href=\"$url\" class=\"edit\">$content</a>",
+            $linkToEditWithProps
+        );
 
     }
 }
