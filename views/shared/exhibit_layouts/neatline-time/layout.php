f39a5450fcefff9f6a06baec540e58ca7cdb0de5
Scott Bailey <csb5t@virginia.edu>
2016-01-26 11:42:36 -0500

    Set center date of timelines in an exhibit

--- views/shared/exhibit_layouts/neatline-time/layout.php
+++ views/shared/exhibit_layouts/neatline-time/layout.php
@@ -1,14 +1,11 @@
 <?php
-if (!array_key_exists('timeline-id', $options)) {
-    return;
-}
-$timeline = get_record_by_id('NeatlineTimeTimeline',  $options['timeline-id']);
-if (!$timeline) {
-    return;
-}
-set_current_record('neatline_time_timeline', $timeline);
-print_r($timeline->center_date);
-// Need to pass through the center date here to make this work in exhibit builder
-// echo $this->partial('timelines/_timeline.php', array('center_date' => metadata($neatline_time_timeline, 'center_date')));
-echo $this->partial('timelines/_timeline.php');
+  if (!array_key_exists('timeline-id', $options)) {
+      return;
+  }
+  $timeline = get_record_by_id('NeatlineTimeTimeline',  $options['timeline-id']);
+  if (!$timeline) {
+      return;
+  }
+  set_current_record('neatline_time_timeline', $timeline);
+  echo $this->partial('timelines/_timeline.php', array('center_date' => metadata($timeline, 'center_date')));
 ?>
