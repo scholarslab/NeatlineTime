8eb693012deb1267c087fde0147b78b5bf6233b8
Scott Bailey <csb5t@virginia.edu>
2016-03-17 14:29:26 -0400

    Refactor date and add image support to the slide
    - Refactor to provide function to create an end date
    - The end date currently does not work and is commented out, along with various logs for debugging
    - If the record has a file, it is returned in the json with the image key. Take what is there and include it in the slide.

--- views/shared/timelines/_timeline.php
+++ views/shared/timelines/_timeline.php
@@ -26,18 +26,23 @@
         var jsonTimelineUri = '<?php echo neatlinetime_json_uri_for_timeline(); ?>';
 
         $.getJSON(jsonTimelineUri, function(data) {
-          console.log('json loc: ', jsonTimelineUri);
-          console.log('data ', data);
+          // console.log('data ', data);
           var timelineEvents = new Array();
+
+          // Since end is reserved by jquery, change the end key to finish
+          var stringData = JSON.stringify(data);
+          var cleanedData = stringData.replace(/"end":/g, '"finishDate":');
+          cleanedData = JSON.parse(cleanedData);
+          console.log(cleanedData);
 
           for (var i = 0; i < data.events.length; i++) {
             // Parse the date string into Y, M, D
             // Assumes YYYY-MM-DD
-            var entryStartDate = data.events[i].start;
-            var parsedDate = entryStartDate.split('-');
-            var entryStartYear = parsedDate[0];
-            var entryStartMonth = parsedDate[1];
-            var entryStartDay = parsedDate[2].slice(0, 2);
+            var startDate = parseDate(cleanedData.events[i].start);
+            // console.log('finishDate: ', cleanedData.events[i][finishDate]);
+            // var endDate = parseDate(cleanedData.events[i].finishDate);
+            // console.log(startDate);
+            // console.log(endDate);
 
             // Create the slide object for the record
             var timelineEntry = {
@@ -46,11 +51,17 @@
                 "text": data.events[i].description,
               },
               "start_date": {
-                  "year": entryStartYear,
-                  "month": entryStartMonth,
-                  "day": entryStartDay
+                  "year": startDate[0],
+                  "month": startDate[1],
+                  "day": startDate[2]
               },
             };
+
+            if (data.events[i].image) {
+              timelineEntry["media"] = { "url": data.events[i].image };
+            }
+
+            console.log(timelineEntry);
             // Add the slide to the events
             timelineEvents.push(timelineEntry);
           }
@@ -68,6 +79,20 @@
 
           // initialize the timeline instance
           window.timeline = new TL.Timeline('timeline-embed', slides);
+
+          function parseDate(entryDateString) {
+            console.log(entryDateString);
+            var entryDate = entryDateString;
+            // console.log('entrydate', entryDateString);
+
+            var parsedDate = entryDate.split('-');
+
+            var entryYear = parsedDate[0];
+            var entryMonth = parsedDate[1];
+            var entryDay = parsedDate[2].slice(0, 2);
+
+            return [entryYear, entryMonth, entryDay];
+          };
 
         });
 
